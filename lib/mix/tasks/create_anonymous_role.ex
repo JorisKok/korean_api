7f1b0404e62c17fe4b652b9199694e5bdb0cf254
JorisKok <joris_kok1@hotmail.com>
2019-06-05 14:46:09 +0200

    Uncomment lines

--- lib/mix/tasks/create_anonymous_role.ex
+++ lib/mix/tasks/create_anonymous_role.ex
@@ -7,17 +7,17 @@ defmodule Mix.Tasks.CreateAnonymousRole do
       |> Enum.each(&Application.ensure_all_started/1)
     KoreanApi.Repo.start_link
 
-#    query = """
-#      create role web_anon nologin;
-#    """
-#
-#    Ecto.Adapters.SQL.query!(KoreanApi.Repo, query)
-#
-#    query = """
-#      grant usage on schema public to web_anon;
-#    """
-#
-#    Ecto.Adapters.SQL.query!(KoreanApi.Repo, query)
+    query = """
+      create role web_anon nologin;
+    """
+
+    Ecto.Adapters.SQL.query!(KoreanApi.Repo, query)
+
+    query = """
+      grant usage on schema public to web_anon;
+    """
+
+    Ecto.Adapters.SQL.query!(KoreanApi.Repo, query)
 
     query = """
       grant select on words to web_anon;
